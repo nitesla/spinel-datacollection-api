1d74e595c4ea468e93a9fc55eab6012689b05921
toluadesola <t.toluadesola@gmail.com>
2022-03-04 15:17:08 +0100

    added controller for CommentDictionary and IndicatorDictionary

--- src/main/java/com/sabi/datacollection/api/controllers/CommentDictionaryController.java
+++ src/main/java/com/sabi/datacollection/api/controllers/CommentDictionaryController.java
@@ -1,10 +1,10 @@
 package com.sabi.datacollection.api.controllers;
 
+import com.sabi.datacollection.core.dto.request.CommentDictionaryDto;
 import com.sabi.datacollection.core.dto.request.EnableDisableDto;
-import com.sabi.datacollection.core.dto.request.SectorDto;
-import com.sabi.datacollection.core.dto.response.SectorResponseDto;
-import com.sabi.datacollection.core.models.Sector;
-import com.sabi.datacollection.service.services.SectorService;
+import com.sabi.datacollection.core.dto.response.CommentDictionaryResponseDto;
+import com.sabi.datacollection.core.models.CommentDictionary;
+import com.sabi.datacollection.service.services.CommentDictionaryService;
 import com.sabi.framework.dto.responseDto.Response;
 import com.sabi.framework.utils.Constants;
 import com.sabi.framework.utils.CustomResponseCode;
@@ -18,55 +18,56 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(Constants.APP_CONTENT+"sector")
-public class SectorController {
+@RequestMapping(Constants.APP_CONTENT+"commentdictionary")
+public class CommentDictionaryController {
 
-    private final SectorService service;
+    private final CommentDictionaryService service;
 
-    public SectorController(SectorService service) {
+
+    public CommentDictionaryController(CommentDictionaryService service) {
         this.service = service;
     }
 
-
-
     @PostMapping("")
-    public ResponseEntity<Response> createSector(@RequestBody SectorDto request) {
+    public ResponseEntity<Response> createCommentDictionary(@RequestBody CommentDictionaryDto commentDictionaryDto) {
         Response response = new Response();
-        SectorResponseDto sectorResponse = service.createSector(request);
+        System.err.println(commentDictionaryDto);
+        CommentDictionaryResponseDto commentDictionaryResponse = service.createCommentDictionary(commentDictionaryDto);
         response.setCode(CustomResponseCode.SUCCESS);
         response.setDescription("Successful");
-        response.setData(sectorResponse);
+        response.setData(commentDictionaryResponse);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
     @PutMapping("")
-    public ResponseEntity<Response> updateSector(@RequestBody SectorDto request) {
+    public ResponseEntity<Response> updateCommentDictionary(@RequestBody CommentDictionaryDto commentDictionaryDto) {
         Response response = new Response();
-        SectorResponseDto sectorResponse = service.updateSector(request);
+        CommentDictionaryResponseDto commentDictionaryResponse = service.updateCommentDictionary(commentDictionaryDto);
+        response.setCode(CustomResponseCode.SUCCESS);
         response.setDescription("Update Successful");
-        response.setData(sectorResponse);
+        response.setData(commentDictionaryResponse);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Response> getSector(@PathVariable Long id) {
+    public ResponseEntity<Response> getCommentDictionary(@PathVariable Long id) {
         Response response = new Response();
-        SectorResponseDto sectorResponse = service.findSectorById(id);
+        CommentDictionaryResponseDto commentDictionaryResponse = service.findCommentDictionaryById(id);
         response.setCode(CustomResponseCode.SUCCESS);
         response.setDescription("Record fetched Successfully");
-        response.setData(sectorResponse);
+        response.setData(commentDictionaryResponse);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @GetMapping("/page")
-    public ResponseEntity<Response> getSectors(@RequestParam(value = "name",required = false)String name,
-                                               @RequestParam(value = "page") Integer page,
-                                               @RequestParam(value = "pageSize") Integer pageSize) {
+    public ResponseEntity<Response> getCommentDictionaries(@RequestParam(value = "name",required = false)String name,
+                                                           @RequestParam(value = "page") Integer page,
+                                                           @RequestParam(value = "pageSize") Integer pageSize) {
         Response response = new Response();
-        Page<Sector> sectorPage = service.findAll(name, PageRequest.of(page, pageSize));
+        Page<CommentDictionary> commentDictionaries = service.findAll(name, PageRequest.of(page, pageSize));
         response.setCode(CustomResponseCode.SUCCESS);
         response.setDescription("Record fetched successfully !");
-        response.setData(sectorPage);
+        response.setData(commentDictionaries);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
@@ -82,10 +83,10 @@ public class SectorController {
     @GetMapping("/active/list")
     public ResponseEntity<Response> getAllByActive(@RequestParam(value = "isActive")Boolean isActive){
         Response response = new Response();
-        List<Sector> sectorPage = service.getAll(isActive);
+        List<CommentDictionary> commentDictionaryList = service.getAll(isActive);
         response.setCode(CustomResponseCode.SUCCESS);
         response.setDescription("Record fetched successfully!");
-        response.setData(sectorPage);
+        response.setData(commentDictionaryList);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
