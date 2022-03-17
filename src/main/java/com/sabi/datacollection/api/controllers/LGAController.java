4bd18e36c2ed48c3376958d50605c9efa5b1b436
Gbeminiyi Ariyo-Agbaje <gbeminiyi.a@spinelconsulting.com>
2022-03-17 21:52:16 +0100

    Update on country, lga & state

--- src/main/java/com/sabi/datacollection/api/controllers/LGAController.java
+++ src/main/java/com/sabi/datacollection/api/controllers/LGAController.java
@@ -1,18 +1,22 @@
 package com.sabi.datacollection.api.controllers;
 
 
+import com.sabi.datacollection.core.dto.request.EnableDisableDto;
+import com.sabi.datacollection.core.dto.request.LGADto;
+import com.sabi.datacollection.core.dto.response.LGAResponseDto;
+import com.sabi.datacollection.core.models.LGA;
 import com.sabi.datacollection.service.services.LGAService;
-import com.sabi.framework.globaladminintegration.GlobalService;
-import com.sabi.framework.globaladminintegration.request.BankRequest;
-import com.sabi.framework.globaladminintegration.request.SingleRequest;
-import com.sabi.framework.globaladminintegration.response.ListResponse;
-import com.sabi.framework.globaladminintegration.response.PageResponse;
-import com.sabi.framework.globaladminintegration.response.SingleResponse;
+import com.sabi.framework.dto.responseDto.Response;
 import com.sabi.framework.utils.Constants;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.sabi.framework.utils.CustomResponseCode;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @SuppressWarnings("All")
 @RestController
@@ -21,9 +25,6 @@ public class LGAController {
 
 
     private final LGAService service;
-
-    @Autowired
-    private GlobalService globalService;
 
     public LGAController(LGAService service) {
         this.service = service;
@@ -35,17 +36,17 @@ public class LGAController {
      * <remarks>this endpoint is responsible for creation of new lga</remarks>
      */
 
-//    @PostMapping("")
-//    public ResponseEntity<Response> createLga(@Validated @RequestBody LGADto request){
-//        HttpStatus httpCode ;
-//        Response resp = new Response();
-//        LGAResponseDto response = service.createLga(request);
-//        resp.setCode(CustomResponseCode.SUCCESS);
-//        resp.setDescription("Successful");
-//        resp.setData(response);
-//        httpCode = HttpStatus.CREATED;
-//        return new ResponseEntity<>(resp, httpCode);
-//    }
+    @PostMapping("")
+    public ResponseEntity<Response> createLga(@Validated @RequestBody LGADto request){
+        HttpStatus httpCode ;
+        Response resp = new Response();
+        LGAResponseDto response = service.createLga(request);
+        resp.setCode(CustomResponseCode.SUCCESS);
+        resp.setDescription("Successful");
+        resp.setData(response);
+        httpCode = HttpStatus.CREATED;
+        return new ResponseEntity<>(resp, httpCode);
+    }
 
 
 
@@ -55,17 +56,17 @@ public class LGAController {
      * <remarks>this endpoint is responsible for updating lga</remarks>
      */
 
-//    @PutMapping("")
-//    public ResponseEntity<Response> updateLga(@Validated @RequestBody  LGADto request){
-//        HttpStatus httpCode ;
-//        Response resp = new Response();
-//        LGAResponseDto response = service.updateLga(request);
-//        resp.setCode(CustomResponseCode.SUCCESS);
-//        resp.setDescription("Update Successful");
-//        resp.setData(response);
-//        httpCode = HttpStatus.OK;
-//        return new ResponseEntity<>(resp, httpCode);
-//    }
+    @PutMapping("")
+    public ResponseEntity<Response> updateLga(@Validated @RequestBody  LGADto request){
+        HttpStatus httpCode ;
+        Response resp = new Response();
+        LGAResponseDto response = service.updateLga(request);
+        resp.setCode(CustomResponseCode.SUCCESS);
+        resp.setDescription("Update Successful");
+        resp.setData(response);
+        httpCode = HttpStatus.OK;
+        return new ResponseEntity<>(resp, httpCode);
+    }
 
 
     /** <summary>
@@ -73,36 +74,16 @@ public class LGAController {
      * </summary>
      * <remarks>this endpoint is responsible for getting a single record</remarks>
      */
-//    @GetMapping("/{id}")
-//    public ResponseEntity<Response> getLga(@PathVariable Long id){
-//        HttpStatus httpCode ;
-//        Response resp = new Response();
-//        LGAResponseDto response = service.findLga(id);
-//        resp.setCode(CustomResponseCode.SUCCESS);
-//        resp.setDescription("Record fetched successfully !");
-//        resp.setData(response);
-//        httpCode = HttpStatus.OK;
-//        return new ResponseEntity<>(resp, httpCode);
-//    }
-
-
-    @PostMapping("")
-    public SingleResponse getLga (SingleRequest request) throws Exception {
-        SingleResponse response= globalService.getSingleLga(request);
-        return response;
-    }
-
-
-    @PostMapping("/page")
-    public PageResponse getLgas (BankRequest request) throws Exception {
-        PageResponse response= globalService.getLgaPagination(request);
-        return response;
-    }
-
-    @PostMapping("/list")
-    public ListResponse getAll (BankRequest request) throws Exception {
-        ListResponse response= globalService.getLgaList(request);
-        return response;
+    @GetMapping("/{id}")
+    public ResponseEntity<Response> getLga(@PathVariable Long id){
+        HttpStatus httpCode ;
+        Response resp = new Response();
+        LGAResponseDto response = service.findLga(id);
+        resp.setCode(CustomResponseCode.SUCCESS);
+        resp.setDescription("Record fetched successfully !");
+        resp.setData(response);
+        httpCode = HttpStatus.OK;
+        return new ResponseEntity<>(resp, httpCode);
     }
 
     /** <summary>
@@ -110,20 +91,20 @@ public class LGAController {
      * </summary>
      * <remarks>this endpoint is responsible for getting all records and its searchable</remarks>
      */
-//    @GetMapping("/page")
-//    public ResponseEntity<Response> getLgas(@RequestParam(value = "name",required = false)String name,
-//                                            @RequestParam(value = "stateId",required = false)Long stateId,
-//                                              @RequestParam(value = "page") int page,
-//                                              @RequestParam(value = "pageSize") int pageSize){
-//        HttpStatus httpCode ;
-//        Response resp = new Response();
-//        Page<LGA> response = service.findAll(name,stateId, PageRequest.of(page, pageSize));
-//        resp.setCode(CustomResponseCode.SUCCESS);
-//        resp.setDescription("Record fetched successfully !");
-//        resp.setData(response);
-//        httpCode = HttpStatus.OK;
-//        return new ResponseEntity<>(resp, httpCode);
-//    }
+    @GetMapping("/page")
+    public ResponseEntity<Response> getLgas(@RequestParam(value = "name",required = false)String name,
+                                            @RequestParam(value = "stateId",required = false)Long stateId,
+                                              @RequestParam(value = "page") int page,
+                                              @RequestParam(value = "pageSize") int pageSize){
+        HttpStatus httpCode ;
+        Response resp = new Response();
+        Page<LGA> response = service.findAll(name,stateId, PageRequest.of(page, pageSize));
+        resp.setCode(CustomResponseCode.SUCCESS);
+        resp.setDescription("Record fetched successfully !");
+        resp.setData(response);
+        httpCode = HttpStatus.OK;
+        return new ResponseEntity<>(resp, httpCode);
+    }
 
 
 
@@ -133,40 +114,40 @@ public class LGAController {
      * <remarks>this endpoint is responsible for enabling and disenabling a State</remarks>
      */
 
-//    @PutMapping("/enabledisenable")
-//    public ResponseEntity<Response> enableDisEnable(@Validated @RequestBody EnableDisEnableDto request){
-//        HttpStatus httpCode ;
-//        Response resp = new Response();
-//        service.enableDisEnableState(request);
-//        resp.setCode(CustomResponseCode.SUCCESS);
-//        resp.setDescription("Successful");
-//        httpCode = HttpStatus.OK;
-//        return new ResponseEntity<>(resp, httpCode);
-//    }
+    @PutMapping("/enabledisenable")
+    public ResponseEntity<Response> enableDisEnable(@Validated @RequestBody EnableDisableDto request){
+        HttpStatus httpCode ;
+        Response resp = new Response();
+        service.enableDisEnableState(request);
+        resp.setCode(CustomResponseCode.SUCCESS);
+        resp.setDescription("Successful");
+        httpCode = HttpStatus.OK;
+        return new ResponseEntity<>(resp, httpCode);
+    }
 
 
-//    @GetMapping("/list")
-//    public ResponseEntity<Response> getAll(@RequestParam(value = "stateId",required = false)Long stateId){
-//        HttpStatus httpCode ;
-//        Response resp = new Response();
-//        List<LGA> response = service.getAllByStateId(stateId);
-//        resp.setCode(CustomResponseCode.SUCCESS);
-//        resp.setDescription("Record fetched successfully !");
-//        resp.setData(response);
-//        httpCode = HttpStatus.OK;
-//        return new ResponseEntity<>(resp, httpCode);
-//    }
+    @GetMapping("/list")
+    public ResponseEntity<Response> getAll(@RequestParam(value = "stateId",required = false)Long stateId){
+        HttpStatus httpCode ;
+        Response resp = new Response();
+        List<LGA> response = service.getAllByStateId(stateId);
+        resp.setCode(CustomResponseCode.SUCCESS);
+        resp.setDescription("Record fetched successfully !");
+        resp.setData(response);
+        httpCode = HttpStatus.OK;
+        return new ResponseEntity<>(resp, httpCode);
+    }
 
-//    @GetMapping("/active/list")
-//    public ResponseEntity<Response> getAllByActive(@RequestParam(value = "isActive",required = false)Boolean isActive){
-//        HttpStatus httpCode ;
-//        Response resp = new Response();
-//        List<LGA> response = service.getAll(isActive);
-//        resp.setCode(CustomResponseCode.SUCCESS);
-//        resp.setDescription("Record fetched successfully !");
-//        resp.setData(response);
-//        httpCode = HttpStatus.OK;
-//        return new ResponseEntity<>(resp, httpCode);
-//    }
+    @GetMapping("/active/list")
+    public ResponseEntity<Response> getAllByActive(@RequestParam(value = "isActive",required = false)Boolean isActive){
+        HttpStatus httpCode ;
+        Response resp = new Response();
+        List<LGA> response = service.getAll(isActive);
+        resp.setCode(CustomResponseCode.SUCCESS);
+        resp.setDescription("Record fetched successfully !");
+        resp.setData(response);
+        httpCode = HttpStatus.OK;
+        return new ResponseEntity<>(resp, httpCode);
+    }
 
 }
